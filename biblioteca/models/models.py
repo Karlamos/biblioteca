84b17b9e031dd720ba51f7ba00cce2a047986cef
Karlamos <mosqueraalmeida2006@gmail.com>
2025-10-08 10:06:52 -0500

    Menú Autor

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -3,16 +3,31 @@
 from odoo import models, fields, api
 
 class biblioteca_libro(models.Model):
-     _name = 'biblioteca.libro'
-     _description = 'biblioteca.biblioteca'
+    _name = 'biblioteca.libro'
+    _description = 'biblioteca.biblioteca'
+    _rec_name= 'firstname'
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    firstname = fields.Char()
+    author = fields.Many2one('biblioteca.autor', string= 'Autor Libro')
+    value = fields.Integer(string='Numero ejemplares')
+    value2 = fields.Float(compute="_value_pc", store=True, string ='Costo')
+    description = fields.Text(string='Resumen libro')
 
-     @api.depends('value')
-     def _value_pc(self):
-         for record in self:
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
              record.value2 = float(record.value) / 100
 
+
+class biblioteca_Autor(models.Model):
+    _name ='biblioteca.autor'
+    _description = 'biblioteca.autor'
+    _rec_name = 'firstname'
+    
+    firstname = fields.Char()
+    lastname = fields.Char()
+    
+    @api.depends('firstname', 'lastname')
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name =f"{record.firstname} - {record.lastname}"
